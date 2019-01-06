13cd9e3edbe96e6772488b29c1c4975093b3c460
Nick de Rozarieux <banterstudios@gmail.com>
2019-01-06 13:46:46 +0000

    fixing es6 module building for dev and storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = (baseConfig, env, defaultConfig) => ({
+  ...defaultConfig,
+  module: {
+    ...defaultConfig.module,
+    rules: [
+      // Temp fix for issue: https://github.com/storybooks/storybook/issues/3346
+      ...defaultConfig.module.rules.filter(rule => !(
+        (rule.use && rule.use.length && rule.use.find(({ loader }) => loader === 'babel-loader'))
+      )),
+      {
+        test: /\.jsx?$/,
+        include: require('path').resolve('./'), // eslint-disable-line global-require
+        exclude: /(node_modules|dist)/,
+        loader: 'babel-loader',
+      }
+    ],
+  },
+});
