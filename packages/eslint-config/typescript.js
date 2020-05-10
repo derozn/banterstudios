01b4812bb5ffc924eb794e598ed658affccce033
Nick <banterstudios@gmail.com>
2020-05-11 00:14:08 +0100

    Fix typescript lint

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -1,10 +1,12 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
+    "@typescript-eslint/recommended",
     "eslint-config-airbnb-typescript",
     "eslint-config-prettier",
+    "prettier/@typescript-eslint",
     "./rules/main",
-    "./rules/prettier"
+    "./rules/prettier",
   ].map(require.resolve),
   plugins: ["jest"],
   env: {
