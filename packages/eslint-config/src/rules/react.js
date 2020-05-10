24e583cf55146c45fb46fa6877c6777818498c69
Nick <banterstudios@gmail.com>
2020-05-10 15:54:09 +0100

    Add self closing tag rule to react

--- packages/eslint-config/src/rules/react.js
+++ packages/eslint-config/src/rules/react.js
@@ -7,6 +7,14 @@ module.exports = {
     // We have some examples of next JS links that are not valid a tags
     "jsx-a11y/anchor-is-valid": "off",
     // In React world, we allow default exports.
-    "import/no-default-export": "off"
+    "import/no-default-export": "off",
+    // Self close tags
+    "react/self-closing-comp": [
+      "error",
+      {
+        "component": true,
+        "html": true
+      }
+    ]
   }
 };
