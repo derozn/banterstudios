ca9a6d30446a8fa58058794ba655b40b355b0e78
Nick <banterstudios@gmail.com>
2020-05-11 01:39:36 +0100

    more typescript changes

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -1,13 +1,14 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "jest"],
   extends: [
     "eslint-config-airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
     "eslint-config-prettier",
     "eslint-config-prettier/@typescript-eslint",
     "./rules/main",
     "./rules/prettier",
   ].map(require.resolve),
-  plugins: ["@typescript-eslint", "jest"],
   env: {
     "jest/globals": true,
     browser: true
