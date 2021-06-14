9806937c58424f4bd78feac8b3e9a9e35b373cd4
Nick <banterstudios@gmail.com>
2021-06-14 19:07:56 +0100

    update config react@

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -3,7 +3,6 @@ module.exports = {
     "eslint-config-airbnb",
     "eslint-config-airbnb/hooks",
     "eslint-config-prettier",
-    "eslint-config-prettier/react",
     "./rules/main",
     "./rules/prettier",
     "./rules/react",
